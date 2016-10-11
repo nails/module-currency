45abfdd212a31c0023026706a407d68c47dda184
Pablo de la Peña <p@shedcollective.org>
2016-10-11 10:25:55 +0100

    Initial commit

---
+++ src/Exception/CurrencyException.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Currency Exception
+ *
+ * @package     Nails
+ * @subpackage  module-currency
+ * @category    Exceptions
+ * @author      Nails Dev Team
+ * @link
+ */
+
+namespace Nails\Currency\Exception;
+
+use Nails\Common\Exception\NailsException;
+
+class CurrencyException extends NailsException
+{
+}
