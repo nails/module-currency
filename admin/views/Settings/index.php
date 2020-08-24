e556720c53a85f1f3662d8eab9f37154dd4b87ca
Pablo de la Peña <p@shedcollective.org>
2020-08-24 16:05:24 +0100

    Rendering currency exchange data in admin

--- admin/views/Settings/index.php
+++ admin/views/Settings/index.php
@@ -31,6 +31,63 @@ $oInput = \Nails\Factory::service('Input');
                 );
             },
         ] : null,
+        [
+            'label'   => 'Exchange Rates',
+            'content' => function () use ($aSupported, $aSettings) {
+
+                if (!empty($aSettings['currency_matrix_updated'])) {
+                    ?>
+                    <p class="alert alert-info">
+                        Exchange rates were last updated: <?=niceTime($aSettings['currency_matrix_updated'])?>
+                        (<?=toUserDatetime($aSettings['currency_matrix_updated'])?>)
+                    </p>
+                    <?php
+                }
+                if (!empty($aSettings['currency_matrix'])) {
+                    ?>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Currency</th>
+                                <?php
+                                foreach ($aSettings['currency_matrix'] as $sCurrency => $aMatrix) {
+                                    ?>
+                                    <th><?=$sCurrency?></th>
+                                    <?php
+                                }
+                                ?>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            foreach ($aSettings['currency_matrix'] as $sCurrency => $aMatrix) {
+                                ?>
+                                <tr>
+                                    <td><?=$sCurrency?></td>
+                                    <?php
+                                    foreach ($aMatrix as $sCurrencyChild => $fRate) {
+                                        ?>
+                                        <td><?=$fRate?></td>
+                                        <?php
+                                    }
+                                    ?>
+                                </tr>
+                                <?php
+                            }
+                            ?>
+                        </tbody>
+                    </table>
+                    <?php
+                } else {
+                    ?>
+                    <p class="alert alert-warning">
+                        <strong>No currency exchange data is avaialble.</strong>
+                        <br>Update the exchange matrix by executing <code>nails currency:update:exchangerate</code>
+                    </p>
+                    <?php
+                }
+            },
+        ],
     ]));
 
     ?>
