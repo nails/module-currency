de6ba43065fa16e517bd2a0db1c5061767d7c5d0
Pablo de la Peña <p@shedcollective.org>
2023-07-03 10:45:49 +0100

    feat: Adds missing exception

---
+++ src/Exception/ExchangeException/DriverApiException.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * DriverApiException
+ *
+ * @package     Nails
+ * @subpackage  module-currency
+ * @category    Exceptions
+ * @author      Nails Dev Team
+ */
+
+namespace Nails\Currency\Exception\ExchangeException;
+
+use Nails\Currency\Exception\ExchangeException;
+
+class DriverApiException extends ExchangeException
+{
+}
